c6ced2a7a75b41da0cfdddc4cd79cbd0a9a0cd42
mirrorecho <info@randallwest.com>
2015-11-22 17:03:53 -0600

    basic verse

---
+++ _settings.py
@@ -0,0 +1,11 @@
+import sys
+
+# replace with local path...
+ROOT_PATH = "/home/randall/Code/mirrorecho" 
+sys.path.append(ROOT_PATH)
+
+# replace with project name...
+PROJECT_NAME = "rwestmusic-narcissapark"
+
+# relpace local path / project name...
+PROJECT_PATH = "/home/randall/Code/mirrorecho/rwestmusic-narcissapark"
--- park.py
+++ park.py
@@ -1,4 +1,4 @@
-import settings
+import _settings
 from abjad import *
 from calliope.bubbles import *
 
@@ -31,29 +31,30 @@ class SongScore(BubbleScore):
             instrument=instrumenttools.BaritoneVoice(instrument_name="Frump", short_instrument_name="F."), 
             clef="bass",
             commands=(
-                ("""addlyrics { 
+                # ("""addlyrics { 
                     
-                    What a lark
-                    Fun and games from dawn past dark
-                    Huge projections
-                    Huge reflections
-                    Astroturf in all directions
+                #     What a lark
+                #     Fun and games from dawn past dark
+                #     Huge projections
+                #     Huge reflections
+                #     Astroturf in all directions
 
-                    Musak bland but very loud (loud boring chords here)
-                    It’s sure to draw a raucous crowd
-                    Andrew Weber, Philip Glass
-                    No one will even miss the grass
+                #     Musak bland but very loud (loud boring chords here)
+                #     It’s sure to draw a raucous crowd
+                #     Andrew Weber, Philip Glass
+                #     No one will even miss the grass
 
-                    Lights that flash and search the sky
-                    Drones o’re head will dive and fly
-                    Never mind old Burnham’s promise
-                    Forget the war and the Islamists
+                #     Lights that flash and search the sky
+                #     Drones o’re head will dive and fly
+                #     Never mind old Burnham’s promise
+                #     Forget the war and the Islamists
 
-                    Come and play 
-                    Ev-er-y night and day
-                    At our Narcissapark!
+                #     Come and play 
+                #     Ev-er-y night and day
+                #     At our Narcissapark!
                     
-                } """, "after"),)
+                # } """, "after"),
+                )
             )
     vinkle = BubbleStaff(instrument=instrumenttools.BaritoneVoice(instrument_name="Vinkle", short_instrument_name="V."), clef="bass")
     # sequence = ("frump_staff","vinkle","piano")
@@ -81,6 +82,25 @@ class SongPhrase(Song):
     frump = RestPhrase()
     vinkle = RestPhrase()
 
+piano_lick1 = Line("r4 <c' e' g'>4 <c' e' g'>4 | r4 <c' e' g'>8 <c' e' g'>8 <c' e' g'>4 | ")
+piano_hanker = Line("<g' c''>4 <g' c''>4 <g' c''>4")
+
+class PianoWalkUp(SongPhrase):
+    piano1 = piano_lick1 + piano_lick1 + piano_hanker + piano_hanker + piano_hanker + piano_hanker
+    piano2 = Line("c4 r4 r4 | g,4 r4 r4 | c4 r4 r4 | d4 r4 r4 |     a,4 r4 b,4 | e4 r4 b,4 | fs4 c4 fs4 | gs4 bs4 ds4  ")
+
+class LarkPhrase(PianoWalkUp):
+    frump = Line("g2 e4 | c4 r4 r4 | e2 g4 | d2 g4 | g2 g4 | g4 r4 r4 | R2. | R2.")
+
+class HugePhrase(SongPhrase):
+    vinkle = Line("af2 f4 | df4 df4 r4 | f2 af4 | ef4 ef2 | R2. | R2. | R2. | R2. ")
+    piano1 = Transpose(PianoWalkUp.piano1, transpose_expr=1)
+    piano2 = Transpose(PianoWalkUp.piano2, transpose_expr=1)
+
+class AstroturfPhrase(SongPhrase):
+    frump = Line("a2 fs4 | d4 r4 e4 | e2 a4 | a4 a2 | R2. | R2. | R2. | R2.")
+    piano1 = Transpose(PianoWalkUp.piano1, transpose_expr=2)
+    piano2 = Transpose(PianoWalkUp.piano2, transpose_expr=2)
 
 # class SomeMusic1(Song):
 #     # piano1 = PolyLine("a'1 a'1", "g'2 g'2 g'2 g'2", "b2. b4 b1")
@@ -101,7 +121,7 @@ class SongPhrase(Song):
     # frump = SomeMusic.frump.change_lyrics("Oh my can't you see me?")
 
 class SongMusic(GridSequence, Song):
-    grid_sequence = (SongStart, SongPhrase )
+    grid_sequence = (SongStart, LarkPhrase, HugePhrase, AstroturfPhrase )
 
 # class Lick1(Song):
 #     piano1 = Line("r1\\fermata")
